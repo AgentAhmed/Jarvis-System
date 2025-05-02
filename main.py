33fec18dae6006f932c9ff8fefa4c36e900923ec
Ahmed <ahmed.mca18@gmail.com>
2025-05-02 12:10:37 +0530

    update functions

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import speech_recognition as sr 
 import pyttsx3
+import logging
+import os 
+
+# This is Logger for the application
+LOG_DIR = "logs"
+LOG_FILE_NAME = "application.log"
+
+os.makedirs(LOG_DIR,exist_ok = True)
+
+log_path = os.path.join(LOG_DIR,LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename=log_path,
+    format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
+    level= logging.INFO,
+)
+
 
 # Taking the male voice from my system
 
@@ -13,5 +30,30 @@ print(voices)
 
 engine.setProperty('voice',voices[0].id)
 
-engine.say("Hello , How can I assist you ?")
-engine.runAndWait()
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+    
+# speak("Hello , I am Jarvis. How can I assist you.") 
+
+def takeCommand():
+    r = sr.Recognizer()   
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source) 
+        
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language="en-in")
+        print(f"User said : {query}\n")
+    except Exception as e:
+        logging.info(e)
+        print("Say that again please")
+        return "None"
+    return query
+
+text = takeCommand()
+speak(text)           
+        
+        
